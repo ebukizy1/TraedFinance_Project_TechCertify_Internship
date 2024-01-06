c28db83ded476a1bed78dcf1089513c0139cc889
ebukizy1 <ebukizy1@gmail.com>
2024-01-06 16:57:37 +0100

    first commit

---
+++ src/main/java/com/TraedFinance_TechCertify/TraedFinance_Project/TraedFinanceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.TraedFinance_TechCertify.TraedFinance_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraedFinanceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraedFinanceProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TraedFinance_TechCertify/TraedFinance_Project/TraedFinanceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TraedFinance_TechCertify.TraedFinance_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraedFinanceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
